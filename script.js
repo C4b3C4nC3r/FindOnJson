ff2538290185875e5948aa7b67591eb0a4b1e327
Livington <livingtonllopezc124@gmail.com>
2022-10-18 21:47:33 -0500

    creacion de new finction para el desarrollo, proximanete creacion de clases para trabajar deinamicamente

--- script.js
+++ script.js
@@ -7,37 +7,6 @@ let fs = require('fs');
 
 
 
-/**
- * 
- * Vars:
- * find ->  busqueda, alone string
- * dimension -> cual es la dimension del file.json int
- * result -> [
- *  {
- *      "ubicacion":[int, string],
- *       "valor":[51515]
- *        "copia":{copia del json}
- * }
- * ]
- * 
- */
-/**
- * 
-    let data = null
-    let dim = 0;
-    let result = [{
-        "ubic":[0,null],
-        "value":[null],
-        "copy": data
-    }]
- */
-
-/**
- * 
- * LOAD: JSONS
- * 
- */
-
 json1d = fs.readFileSync("./json-Example/1d.json","utf-8")//unidimencional
 json2d = fs.readFileSync("./json-Example/2d.json","utf-8")//bidimencional
 json3d = fs.readFileSync("./json-Example/3d.json","utf-8")//tridimencional
@@ -46,37 +15,60 @@ json5d = fs.readFileSync("./json-Example/5d.json","utf-8")//quintadimencional
 
 //console.log(json1d);
 
-function firstd(json,find) {
+function fejd(data,key, find, valor, index) {
     //console.log(typeof(json));
     
-    let data = json;
     result = [{
         "ubic":[0,null],
         "value":[null],
-        "copy": data
+        "copy": data,
+        "dim":0
     }]
+
+
+    result[0]["value"] = valor;
+    result[0]["ubic"] = [index,key]
+    result[0]["dim"] =+1   
+
+        
+    return result
+    
+    
+}
+
+function fejr(valor, find){
+
+    
+    console.log(valor);
+}
+
+function descompress (json,find){
+
+    let data = json;
     for (const index in json) {
         if (Object.hasOwnProperty.call(json, index)) {
             const element = json[index];
             for (const key in element) {
                 if (Object.hasOwnProperty.call(element, key)) {
                     const valor = element[key];
-                    result[0]["value"] = valor;
-                    result[0]["ubic"] = [index,key]                                        
-                    if(key == find){
-                        return result;
-                    }   
+                    if(key === find){
+                        return fejd(data,key,find,valor,index)
+
+                    }
+                    
                 }
             }             
         }
     }
    
-    return false;
-    
+
 }
 
-result = firstd(JSON.parse(json1d),"a")
 
+result = descompress(JSON.parse(json2d),"b")
+
+
+//console.log(result);
 
 console.log(
     result?
@@ -85,8 +77,16 @@ console.log(
             [result[0]["ubic"][1]]
         :"Undefined");
 
+/*
+
+*/
+//res = typeof(result[0]["copy"][result[0]["ubic"][0]][result[0]["ubic"][1]])
+//console.log(res);
 //bucle(json2d)
 //bucle(json3d)
 //bucle(json4d)
 //bucle(json5d)
 //(key === find) ?"yes":"no"
+
+
+
