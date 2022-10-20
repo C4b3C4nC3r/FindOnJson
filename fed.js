bb25fa89074bd8d49e09259c32ffc6f646b4b4e7
Livington <livingtonllopezc124@gmail.com>
2022-10-20 16:07:01 -0500

    SyntaxError: Cannot use import statement outside a module ayudenme a soluconarlo :V

---
+++ fed.js
@@ -0,0 +1,33 @@
+
+class Fed{
+
+
+    static fejd(data,key, valor, index) {
+        //console.log(typeof(json));
+        
+        result = [{
+            "ubic":[0,null],
+            "value":[null],
+            "copy": data,
+            "dim":0
+        }]
+    
+    
+        result[0]["value"] = valor;
+        result[0]["ubic"] = [index,key]
+        result[0]["dim"] =+1   
+    
+            
+        return result
+        
+        
+    }
+    
+    static fejr(valor, find){
+        descompress = new Descompress(valor,find)
+    
+        descompress.descompress()
+
+    }
+
+}
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 /**
  * Import
  */
-
-
+import Descompress from "./Descompress.js"
 let fs = require('fs');
 
 
@@ -15,57 +14,9 @@ json5d = fs.readFileSync("./json-Example/5d.json","utf-8")//quintadimencional
 
 //console.log(json1d);
 
-function fejd(data,key, find, valor, index) {
-    //console.log(typeof(json));
-    
-    result = [{
-        "ubic":[0,null],
-        "value":[null],
-        "copy": data,
-        "dim":0
-    }]
+let des = new Descompress(JSON.parse(json2d),"z")
 
-
-    result[0]["value"] = valor;
-    result[0]["ubic"] = [index,key]
-    result[0]["dim"] =+1   
-
-        
-    return result
-    
-    
-}
-
-function fejr(valor, find){
-
-    
-    console.log(valor);
-}
-
-function descompress (json,find){
-
-    let data = json;
-    for (const index in json) {
-        if (Object.hasOwnProperty.call(json, index)) {
-            const element = json[index];
-            for (const key in element) {
-                if (Object.hasOwnProperty.call(element, key)) {
-                    const valor = element[key];
-                    if(key === find){
-                        return fejd(data,key,find,valor,index)
-
-                    }
-                    
-                }
-            }             
-        }
-    }
-   
-
-}
-
-
-result = descompress(JSON.parse(json2d),"b")
+result = des.descompress()
 
 
 //console.log(result);
